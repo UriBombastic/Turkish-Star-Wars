14937857c6c345b44871fa9ff2ef2682c1eb6b9d
uri.harding <uri.herszbaumharding@gmail.com>
2019-11-28 02:39:27 -0600

    Added Health bar I guess

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
     public string name = "Enemy";
-    public float health = 100f;
+    public float initialHealth = 100f;
+    public float health;
+    public Image healthBar;
+    public TextMeshProUGUI nameText;
     private Rigidbody rb;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        health = initialHealth;
+        if (nameText != null) nameText.text = name;
     }
 
 
@@ -22,8 +34,17 @@ public class Enemy : MonoBehaviour, IDamageable
     public void Damage(float damage, Vector3 knockback)
     {
         health -= damage;
+        UpdateHealthBar();
         if (rb != null) rb.AddForce(knockback);
         if (health <= 0) Kill();
+    }
+
+    public void UpdateHealthBar()
+    {
+        if (healthBar == null) return;
+
+        float ratio = health / initialHealth;
+        healthBar.fillAmount = ratio;
     }
 
     public void Kill()
