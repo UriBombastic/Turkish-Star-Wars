d83ebcbba2390496eb5f24e838f6058896698ec5
uri.harding <uri.herszbaumharding@gmail.com>
2019-11-28 01:11:34 -0600

    Added attacks to statemachine, as well as gitignore

--- Assets/Scripts/HeroStateMachine.cs
+++ Assets/Scripts/HeroStateMachine.cs
@@ -7,9 +7,14 @@ public class HeroStateMachine : MonoBehaviour
 
     public float WalkForce = 20f;
     public float JumpForce = 500f;
+    public float DashForce = 200f;
+    public float DashCoolDown = 1.0f;
     public float BasicAttackStartDelay = 0.0f;
     public float BasicAttackSpeed = 2f; //basic attacks per second
+    public float JumpAttackDelay = 1.0f;
+    public float DashAttackDelay = 1.0f; //cooldown invoked by Dash Attack
     private Rigidbody rb;
+    [SerializeField]
     private float h, v;
     public enum State
     {
@@ -53,11 +58,8 @@ public class HeroStateMachine : MonoBehaviour
                 }
 
                 CheckForJump();
+                CheckForAttack();
 
-                if(Input.GetKeyDown(KeyCode.Mouse0))
-                {
-
-                }
                 break;
 
             case State.WALKING:
@@ -67,21 +69,26 @@ public class HeroStateMachine : MonoBehaviour
                 }
 
                 CheckForJump();
+                CheckForDash();
+                CheckForAttack();
                 break;
 
             case State.JUMPING:
 
                 CheckForMove();
-
+                CheckForDash();
+                CheckForJumpAttack();
                 break;
 
             case State.DASHING:
+                CheckForDashAttack();
                 break;
 
             case State.ATTACKING:
                 break;
 
             case State.JUMPATTACK:
+                CheckForMove();
                 break;
 
             case State.DASHATTACK:
@@ -103,17 +110,48 @@ public class HeroStateMachine : MonoBehaviour
         return false;
     }
 
+    void CheckForJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+   void CheckForDash()
+    {
+        if (v == 0) return;
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+            StartCoroutine(DashTiming(v));
+    }
+
+    void CheckForAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(BasicAttackTiming());
+        }
+    }
+
+    void CheckForJumpAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(JumpAttackTiming());
+        }
+    }
+
+    void CheckForDashAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(DashAttackTiming());
+        }
+    }
+
     void Walk(float x, float z)
     {
 
         Vector3 WalkVector = transform.forward * z + transform.right * x;
         rb.AddForce(WalkVector * WalkForce);
-    }
-
-    void CheckForJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Jump();
     }
 
     void Jump()
@@ -122,9 +160,18 @@ public class HeroStateMachine : MonoBehaviour
         state_ = State.JUMPING;
     }
 
+    IEnumerator DashTiming(float z)
+    {
+        state_ = State.DASHING;
+        Vector3 DashVector = transform.forward * z;
+        rb.AddForce(DashVector * DashForce);
+        yield return new WaitForSeconds(DashCoolDown);
+        state_ = State.WALKING;
+    }
+
     void OnCollisionEnter(Collision col)
     {
-        if (state_ == State.JUMPING)
+        if (state_ == State.JUMPING || state_ == State.JUMPATTACK)
             state_ = State.IDLE;
     }
 
@@ -143,4 +190,37 @@ public class HeroStateMachine : MonoBehaviour
         //Something more elaborate here
         Debug.Log("Attack!");
     }
+
+    IEnumerator JumpAttackTiming()
+    {
+        state_ = State.JUMPATTACK;
+        JumpAttack();
+        yield return new WaitForSeconds(JumpAttackDelay);
+        if(state_ == State.JUMPATTACK) state_ = State.JUMPING;
+
+    }
+
+    void JumpAttack()
+    {
+        Debug.Log("JumpAttack");
+    }
+
+
+
+    IEnumerator DashAttackTiming()
+    {
+        state_ = State.DASHATTACK;
+        DashAttack();
+        yield return new WaitForSeconds(DashAttackDelay);
+        state_ = State.IDLE;
+
+    }
+
+    void DashAttack()
+    {
+        Debug.Log("DashAttack");
+    }
+
+  
+
 }
