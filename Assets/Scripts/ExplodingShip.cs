2c7bc7ba2bd4a1f809353e212a06cbf3c0a46ce7
uri.harding <uri.herszbaumharding@gmail.com>
2019-12-25 02:19:13 -0600

    Basic set up of cut scenes

---
+++ Assets/Scripts/ExplodingShip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingShip : MonoBehaviour
+{
+    public Vector3 moveForce;
+    public GameObject explosionParticles;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rb.AddForce(moveForce);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Explode();
+    }
+
+    public void Explode()
+    {
+        explosionParticles.transform.position = transform.position;
+        explosionParticles.SetActive(true);
+        Destroy(gameObject);
+    }
+}
