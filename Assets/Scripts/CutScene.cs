2773b2fbd633bc4395279c91f13a31078713337b
uri.harding <uri.herszbaumharding@gmail.com>
2019-12-25 02:48:38 -0600

    More cutscene stuff

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour
+{
+    public string CutsceneName;
+    int dialogueIndex =0;
+    public string[] dialogueTexts;
+    public float[] dialogueDelays;
+    int subsectionIndex = 0;
+    public GameObject subsections;
+    public float subsectionDelays;
+    //public List<GameObject[]> subsectionObjects;
+    //public List<float[]> subsectionObjectDelays;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
