87a12f7e5c2638d2109c10b467a72bdf1a894ad0
uri.harding <uri.herszbaumharding@gmail.com>
2019-12-26 01:42:13 -0600

    Added GameMaster, reorganized scripts

---
+++ Assets/Scripts/Game Core/CutScene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using TMPro;
+
+public class CutScene : MonoBehaviour
+{
+    public string CutsceneName;
+    public TextMeshProUGUI NarrationTextBox;
+    int dialogueIndex = 0;
+    public string[] dialogueTexts;
+    public float[] dialogueDelays;
+    int subsectionIndex = 0;
+    public GameObject [] subsections;
+    public float [] subsectionDelays;
+    //public List<GameObject[]> subsectionObjects;
+    //public List<float[]> subsectionObjectDelays;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (subsections.Length != subsectionDelays.Length)
+            Debug.LogError("Uh oh! Stinky! Poopy! Subsections length isn't equal to subsection delays length!");
+        if (dialogueTexts.Length != dialogueDelays.Length)
+            Debug.LogError("Uh oh! Stinky! Poopy! Dialogues length isn't equal to dialogue delays length!");
+        InitializeDialogueTexts();
+        StartCoroutine(SectionsSequence());
+        StartCoroutine(TextSequence());
+    }
+
+    public void InitializeDialogueTexts()
+    {
+
+    }
+
+    public IEnumerator SectionsSequence()
+    {
+        for(int i = 0; i < subsectionDelays.Length; i++)
+        {
+            yield return new WaitForSeconds(subsectionDelays[i]);
+        }
+    }
+
+    public IEnumerator TextSequence()
+    {
+        for (int i = 0; i < dialogueDelays.Length; i++)
+        {
+            yield return new WaitForSeconds(dialogueDelays[i]);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Game Core/GameMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static enum Language
+    {
+        EN,
+        SP,
+        TUR,
+        FUK
+    }
+
+    public static Language language;
+}
