0b34a2b3d0f0dc57fa4457d7465886708576f94a
uri.harding <uri.herszbaumharding@gmail.com>
2019-12-27 03:08:48 -0600

    Worked on part 3 of cutscene 1, including adding fucking Whistle by Flo Rida

---
+++ Assets/Scripts/BasicRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRotate : MonoBehaviour
+{
+    public Vector3 moveDirection;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(moveDirection);
+    }
+}
