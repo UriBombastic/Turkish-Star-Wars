bfff6973b54c6062f1e5920292fc2a3fd1a7fca7
uri.harding <uri.herszbaumharding@gmail.com>
2019-12-27 01:27:20 -0600

    Added explosion Sounds

--- Assets/Scripts/Game Core/CutScene.cs
+++ Assets/Scripts/Game Core/CutScene.cs
@@ -20,11 +20,11 @@ public class CutScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (subsections.Length != subsectionDelays.Length)
+        InitializeDialogueTexts();
+     /*   if (subsections.Length != subsectionDelays.Length)
             Debug.LogError("Uh oh! Stinky! Poopy! Subsections length isn't equal to subsection delays length!");
         if (dialogueTexts.Length != dialogueDelays.Length)
-            Debug.LogError("Uh oh! Stinky! Poopy! Dialogues length isn't equal to dialogue delays length!");
-        InitializeDialogueTexts();
+            Debug.LogError("Uh oh! Stinky! Poopy! Dialogues length isn't equal to dialogue delays length!");*/
         StartCoroutine(SectionsSequence());
         StartCoroutine(TextSequence());
     }
