6b7feb2cfa322d88d01b829222917ab9e75be41e
uri.harding <uri.herszbaumharding@gmail.com>
2019-12-31 03:41:06 -0600

    Started Creating Upgrades UI

---
+++ Assets/Scripts/UpgradePlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpgradePlayer : MonoBehaviour
+{
+
+    public int Points;
+    public HeroController player;
+    public TextMeshProUGUI pointsText;
+    public int AtkUpgradeAmount = 20;
+    public int HealthUpgradeAmount = 20;
+    
+    void Awake()
+    {
+        player = FindObjectOfType<HeroController>();
+    }
+
+    void Start()
+    {
+        pointsText.text = Points.ToString();
+    }
+
+    public void UpgradeAttack()
+    {
+
+    }
+
+    public void UpgradeHealth()
+    {
+        player.maxHealth += HealthUpgradeAmount;
+        player.health += HealthUpgradeAmount;
+    }
+
+    public void UpgradeShield()
+    {
+
+    }
+
+}
