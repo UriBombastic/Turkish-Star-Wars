9ebd7554cd8feb9f840966ae9ed262f46b4652ee
uri.harding <uri.herszbaumharding@gmail.com>
2020-06-05 23:53:35 -0500

    Added much demanded volume control

---
+++ Assets/Scripts/Game Core/VolumeControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+/*[System.Serializable]
+public class VolumePack
+{
+    public AudioSource aud;
+    public float volume;
+}*/
+
+public class VolumeControl : MonoBehaviour
+{
+    public float currentVolume;
+    public float maxVolume = 2.0f; //can bring volume up to 200%. I don't see why you'd ever want to do that though.
+    public static float storedVolume = 1.0f;
+    public Slider volumeSlider;
+    public TextMeshProUGUI volumeText;
+    public AudioSource[] allAudioSources;
+    // [SerializeField]
+    //public VolumePack[] audioSourceVolumeTracker;
+    public float[] originalAudioVolumes;
+   
+    void Awake()
+    {
+        InitializeAudioSourceTrackers();
+    }
+
+    void InitializeAudioSourceTrackers()
+    {
+        originalAudioVolumes = new float[allAudioSources.Length];
+        for (int i = 0; i < allAudioSources.Length; i++)
+        {
+            originalAudioVolumes[i] = allAudioSources[i].volume;
+        }
+    }
+
+    void Start()
+    {
+        currentVolume = storedVolume;
+        volumeSlider.value = currentVolume;
+        volumeSlider.maxValue = maxVolume;
+        OnVolumeChanged(currentVolume);
+    }
+
+    void OnDisable()
+    {
+        storedVolume = currentVolume;
+    }
+
+    public void OnVolumeChanged()
+    {
+        OnVolumeChanged(volumeSlider.value);
+    }
+
+    public void OnVolumeChanged(float newVolume)
+    {
+        currentVolume = newVolume;
+        volumeText.text = GameMaster.StandardRounding(currentVolume * 100) + "%";
+        for(int i = 0; i < allAudioSources.Length; i++)
+        {
+            allAudioSources[i].volume = originalAudioVolumes[i] * newVolume;
+        }
+    }
+}
