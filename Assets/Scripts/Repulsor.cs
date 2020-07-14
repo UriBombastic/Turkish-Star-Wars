7593f68be5cfa0931ec6374a96e60f3747b4109c
uri.harding <uri.herszbaumharding@gmail.com>
2020-07-14 00:46:42 -0500

    Refactored pointless mago enemy passive script into two distinct functions

---
+++ Assets/Scripts/Repulsor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repulsor : MonoBehaviour
+{
+    public float repelForce = 200f;
+    public float repelRadius = 5f;
+
+    void Update()
+    {
+        RepelEverything();
+    }
+
+    void RepelEverything()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, repelRadius);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i].transform != this.transform) //don't hit yourself lol
+            {
+                Rigidbody rb = hitColliders[i].GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    Vector3 attackVector = hitColliders[i].transform.position - transform.position;
+                    attackVector.Normalize();
+                    rb.AddForce(attackVector * repelForce);
+                }
+            }
+        }
+    }
+}
