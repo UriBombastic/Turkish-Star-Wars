26a63c0985795ff84ff2b65dfe14605a81474f4b
uri.harding <uri.herszbaumharding@gmail.com>
2020-07-15 00:59:41 -0500

    Honestly, got kind of bored and just went ham with the church scene

---
+++ Assets/Scripts/AudioReverser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioReverser : MonoBehaviour
+{
+    public AudioSource victim;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<HeroController>())
+            victim.pitch = -1;
+    }
+}
