ede42c1d6bdccf1138ecbf5a00aea5c5920a2e74
Uri Herszbaum <uri.herszbaumharding@gmail.com>
2020-10-25 18:25:11 -0500

    Created generic boss script, and began building functionality of golden ninja guardian

---
+++ Assets/Scripts/Enemies/GenericBoss.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericBoss : Enemy
+{
+    protected float selection;
+    public float minAttackTime = 1.5f;
+    public float maxAttackTime = 3.0f;
+
+    //for game continuity
+    public GameObject[] toggleOnDeath;
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(AttackClock());
+    }
+
+    protected IEnumerator AttackClock()
+    {
+        float timeToNextAttack = Random.Range(minAttackTime, maxAttackTime);
+        yield return new WaitForSeconds(timeToNextAttack);
+
+
+        StartCoroutine(Attack());
+        yield return new WaitForSeconds(BasicAttackCooldown); //allow time for attack to execute before even thinking of doing another attack
+        StartCoroutine(AttackClock()); //begin next attack
+
+    }
+    protected override IEnumerator Attack()
+    {
+        SelectAttack();
+        yield return new WaitForEndOfFrame();
+    }
+   
+   protected virtual void SelectAttack()
+    {
+        //This will change in each boss.
+    }
+    //Effectively overwrites enemy aggression state where it attacks automatically
+    protected override void HandleAggression()
+    {
+        HandleDistances();
+        FacePlayer();
+    }
+
+    //Typically, when you kill a boss, things happen. Make said things happen.
+    void ToggleContinuityElements()
+    {
+        for (int i = 0; i < toggleOnDeath.Length; i++)
+            if (toggleOnDeath[i] != null)
+                toggleOnDeath[i].SetActive(!toggleOnDeath[i].activeInHierarchy);
+
+    }
+
+}
--- Assets/Scripts/Enemies/GoldenNinja.cs
+++ Assets/Scripts/Enemies/GoldenNinja.cs
@@ -2,17 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldenNinja : Enemy
+public class GoldenNinja :GenericBoss
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum AttackState
     {
-        
+        NONE,
+        SPINDASH,
+        PROXIMITY,
+        PROJECTILE
     }
 
-    // Update is called once per frame
-    void Update()
+    public AttackState attackState_;
+
+    public float spinDashChance = 0.25f;
+    public float spinDashDamage = 20f;
+    public float spinDashForce = 3000f;
+    public float spinDashStartup = 1.0f;
+    public float spinDashDuration = 0.5f;
+    public float spinSpeed = 0;
+    public float spinAcceleration = 1f;
+    public float spinDashShockwaveReach = 5.0f;
+    public float maxSpinSpeed = 60f;
+
+    protected override void FixedUpdate()
     {
-        
+        base.FixedUpdate();
+        transform.Rotate(new Vector3(0, spinSpeed, 0));
     }
+
+    protected override void SelectAttack()
+    {
+        selection = Random.Range(0, 1);
+        if (selection > spinDashChance)
+        {
+            if (state_ == State.AGGRESSION)
+            {
+
+            }
+            else if (state_ == State.PLAYERINVIEW)
+            {
+
+            }
+        }
+        else
+        {
+            StartCoroutine(SpinDashSequence());
+        }
+    }
+
+    IEnumerator SpinDashSequence()
+    {
+        StartCoroutine(Accelerate());
+        state_ = State.AGGRESSION;
+
+        yield return new WaitForSeconds(spinDashStartup);
+        StopCoroutine(Accelerate());
+        state_ = State.ATTACKING;
+        attackState_ = AttackState.SPINDASH;
+        SpinDashAttack();
+        yield return new WaitForSeconds(spinDashDuration);
+        state_ = State.IDLE;
+        spinSpeed = 0;
+        yield return null;
+    }
+
+    IEnumerator Accelerate()
+    {
+        Debug.Log("Accelerating");
+        while(spinSpeed < maxSpinSpeed)
+        {
+            Debug.Log("Adding Force");
+            spinSpeed += spinAcceleration;
+            yield return new WaitForSeconds(1 / 60);
+        }
+        yield return null;
+    }
+
+    void SpinDashAttack()
+    {
+        Vector3 AttackVector = (playerTransform.position - transform.position).normalized;
+        rb.AddForce(AttackVector * spinDashForce);
+    }
+
+    protected override void HandleAggression()
+    {
+        HandleDistances();
+        //FacePlayer();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (attackState_ == AttackState.SPINDASH && state_ == State.ATTACKING)
+        {
+            //if is player
+            if (collision.transform == playerTransform)
+            {
+                FundamentalAttack(spinDashDamage, spinDashShockwaveReach, 0f, transform);
+                state_ = State.IDLE; //To prevent second collision or whatever
+            }
+        }
+    }
+
 }
